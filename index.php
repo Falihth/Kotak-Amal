f71f7511a0954c8c8c8bd9952f905cc63761284a
Falih TH <falihtaufiqul@gmail.com>
2024-08-31 20:00:44 +0700

    p

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Smartamal</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="landing.style.css" />
+  </head>
+  <body>
+    <nav class="navbar navbar-custom">
+        <div class="container-fluid">
+            <h1 class="navbar-brand">
+                KOTAK AMAL
+            </h1>
+            <img src="background/icon.png" alt="icon" class="logo1">
+            <img src="background/POLIJE.png" alt="icon" class="logo2">
+        </div>
+    </nav>
+    <div class="container">
+    <span class="border-des"></span>
+    <h1 class="des1">
+        Selamat datang!!!
+        di inovasi terbaru dalam dunia filantropi 
+        </h1>
+        <h2 class="des2">
+        Kami menghadirkan kotak amal pintar yang menggunakan teknologi ESP32-CAM, dirancang khusus untuk memudahkan Anda dalam berbagi kebaikan. Kotak amal ini dilengkapi dengan sistem pengawasan canggih yang dapat mendeteksi dan merekam setiap momen saat donasi dilakukan.
+        </h2>
+    <img src="background/amal.png" alt="icon" class="logo3">
+    <a href="login_index.php" class="block1">login</a>
+    <a href="signup_index.php" class="block2">
+        sign up
+</a>
+  <a href="aboutus.php" class="ab">ABOUT US</a>
+</div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+  </body>
+</html>
